e8001bbf1ea2ccd169a841b22a2a9bc20be95b5b
Sapfir0 <sapfir999999@yandex.ru>
2020-03-22 15:36:06 +0400

    Добавил создание чатиков на фронте

--- public/js/main.js
+++ public/js/main.js
@@ -3,28 +3,56 @@ document.addEventListener('DOMContentLoaded', start);
 const serverUrl = "/server/ajax.php";
 import { post } from "./ajaxHelper.js";
 
+var lastMessageDate;
+
 function start() {
     const sendButton = document.getElementById("sendBtn");
 
+    const timerId = setInterval(() => {
+        // get all messages by chat id
+    }, 1000)
+
     sendButton.addEventListener('click', async () => {
         const messageText = document.getElementById("messageInput").value;
+        const errorBlock = document.getElementsByClassName('error')[0];
+        if (!messageText.match(".+") ) {
+            showError(errorBlock, "Почему сообщение пустое")
+            return
+        }
+        else  {
+            hideError(errorBlock)
+        }
+
         insertMessageToHtml(messageText, true);
 
-        await addMessage(messageText);
+
+        await addMessageToDB(messageText);
     })
 }
 
-function insertMessageToHtml(messageText, isMyMessage) {
+function showError(element, errorString) {
+    element.innerHTML = errorString;
+    element.className = 'error'
+}
+
+function hideError(element) {
+    element.innerHTML = "";
+    element.className = 'clear'
+}
+
+function insertMessageToHtml(messageText, isMyMessage, dateTime) {
     const chat = document.getElementById('chat');
     const messageClass = isMyMessage ? "my-message" : "other-message";
-    const html = `<div class='${messageClass}'> ${messageText} </div>`;
+    const currentDate = new Date();
+    const parsedDate = `${currentDate.getHours()}ч ${currentDate.getMinutes()}м ${currentDate.getSeconds()}с`;
+    const html = `<div class='${messageClass}'> ${messageText} ${parsedDate} </div>`;
     chat.insertAdjacentHTML('beforeend', html);
 }
 
-async function addMessage(messageText, userId, chatId) {
+async function addMessageToDB(messageText, userId, chatId) {
     return await post(serverUrl , {"text": messageText, "request_action" : "sendMessage"});
 }
 
-async function getMessages(userId, chatId) {
-    return await post(serverUrl,{"request_action" : "gesMessages"});
+async function getMessagesFromDB(userId, chatId) {
+    return await post(serverUrl,{"request_action" : "getMessages"});
 }
